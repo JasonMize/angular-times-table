f3e5dcf3852c1a2415806a5b8dcc1eab9b85dcb2
Jason Mize <jasonemize@gmail.com>
2016-10-04 13:37:07 -0400

    display table value in well below table

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ angular.module('app', [])
 function mainController ($scope) {
     $scope.userRows = 12;
     $scope.userCols = 12;
+    $scope.selectedValue = null;
+
 
     $scope.getRows = function (){
         return R.range(1, $scope.userRows+1);
@@ -21,7 +23,16 @@ function mainController ($scope) {
         return $scope.userRows > 0 && $scope.userCols > 0;
     }
 
+    $scope.setSelectedValue = function (row, col) {
+        $scope.selectedValue = {
+            row: row,
+            col: col,
+        }
+    }
 
+    $scope.clearSelectedValue = function () {
+        $scope.selectedValue = null;
+    }
 
 }
 
